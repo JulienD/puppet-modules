9b33b289eb47884a91ed2aa20b825d5500ea7715
Julien Dubreuil <dubreuil.julien@gmail.com>
2012-05-29 16:17:16 +0200

    updated apache manifest

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -4,8 +4,6 @@
 #
 # Parameters:
 #
-# Actions:
-#
 # Requires:
 #
 # Sample Usage:
@@ -29,15 +27,6 @@ class apache::config {
 
   apache::module {"rewrite": }
 
-  /*
-  # TODO : Faire une boucle pour l"import de plusieurs vhost et utiliser un fichier de settings.
-  apache::vhost {
-    "my-site1.com": port => "80", docroot => "/var/www/my-site1", name => "my-site1";
-    "my-site2.com": port => "80", docroot => "/var/www/my-site2", name => "my-site2";
-    "my-site3.com": port => "80", docroot => "/var/www/my-site3", name => "my-site3";
-  }
-  */
-
   # Info directory
   file { "server_info_dir":
     path    => "/var/www/${server_info_dir}/",
@@ -50,3 +39,11 @@ class apache::config {
 class apache {
   include apache::install, apache::config
 }
+
+/*
+  apache::vhost {
+    "my-site1.com": port => "80", docroot => "/var/www/my-site1", name => "my-site1";
+    "my-site2.com": port => "80", docroot => "/var/www/my-site2", name => "my-site2";
+    "my-site3.com": port => "80", docroot => "/var/www/my-site3", name => "my-site3";
+  }
+*/
