67b0dfc5788868ae49c11e3a9f60ec52f93bafbb
Julien Dubreuil <dubreuil.julien@gmail.com>
2012-05-29 16:55:33 +0200

    added pear installation

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,16 +1,27 @@
 # Class: php
 #
-# This class installs Php
-# 
-class php {
+#   This class installs php5.
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class php::install {
 
   $packages = ["libapache2-mod-php5", "php5", "php5-cli", "php5-mysql", "php5-curl", "php5-dev", "php5-mcrypt", "php5-gd", "libpcre3-dev"]
 
   package { $packages:
     ensure => present,
   }
+}
 
-  file { 
+class php::config {
+
+  file {
     "php_a2_ini":
       path => "/etc/php5/apache2/php.ini",
       ensure => present,
@@ -18,7 +29,7 @@ class php {
       owner => 'root',
       group => 'root',
       require => Package['php5', 'libapache2-mod-php5'];
-    
+
     "php_cli_ini":
       path => "/etc/php5/cli/php.ini",
       ensure => present,
@@ -27,11 +38,18 @@ class php {
       group => 'root',
       require => Package['php5', 'php5-cli'];
 
+    # FIXME : Check if this directory is created, beacause there is a dependency with the apache class.
     "phpinfo":
-      path    => "/var/www/info/index.php",
+      path    => "/var/www/${server_info_dir}/index.php",
       content => "<?php phpinfo(); ?>",
       ensure  => present,
       notify  => Package['php5'],
-      require => File['server_info_dir'];  
+      require => File['server_info_dir'];
   }
 }
+
+class php {
+  include php::install, php::config
+  include php::pear
+}
+
