bb13006acc077aa4af0577d1130908b737ad739c
Julien Dubreuil <dubreuil.julien@gmail.com>
2012-05-29 16:54:19 +0200

    updated phpmyadmin class

--- modules/phpmyadmin/manifests/init.pp
+++ modules/phpmyadmin/manifests/init.pp
@@ -1,20 +1,35 @@
+# Class: phpmyadmin
 #
-# Installation de phpmyadmin
+#   This class installs phpmyadmin software.
 #
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class phpmyadmin::install {
+  package { "phpmyadmin" :
+    ensure => present,
+    require => Class["mysql", "php"],
+  }
+}
+
+class phpmyadmin::config {
+  file { "a2en_phpmyadmin":
+    name    => "/etc/apache2/sites-enabled/phpmyadmin",
+    ensure  => link,
+    target  => "/etc/phpmyadmin/apache.conf",
+    require => Class["phpmyadmin::install"],
+  }
+
+  exec { "sudo apache2ctl graceful" :
+    require => File["a2en_phpmyadmin"],
+  }
+}
+
 class phpmyadmin {
-    package { "phpmyadmin" :
-        ensure => present,
-        require => Package["mysql-server", "php5"],
-    }
-
-    file { [ "/etc/apache2/sites-enabled/phpmyadmin" ] :
-        ensure => link,
-        target => "/etc/phpmyadmin/apache.conf",
-        require => Package["phpmyadmin", "httpd"],
-    }
-
-    exec { "sudo apache2ctl graceful" :
-        path => "/usr/bin:/usr/sbin:/bin",
-        require => Package["httpd"],
-    }
+  include phpmyadmin::install, phpmyadmin::config
 }
