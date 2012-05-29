b6432f9404d8f907c742ff3d9c1b0208aa3a4ce4
Julien Dubreuil <dubreuil.julien@gmail.com>
2012-05-29 16:24:26 +0200

    updated bootstrap phase file

--- modules/bootstrap/manifests/init.pp
+++ modules/bootstrap/manifests/init.pp
@@ -1,6 +1,14 @@
+#
 # Class: bootstrap
 #
-# This class is run at first to initialize the configuration of the server and before modules provisionnement.
+#   This class is run at first to initialize the configuration of the server and
+#   before modules provisionnement.
+#
+# Parameters:
+#
+# Requires:
+#
+# Sample Usage:
 #
 class bootstrap {
 
@@ -12,9 +20,11 @@ class bootstrap {
   exec { "apt-get update":
     command => "/usr/bin/apt-get update",
   }
+
   Package {
     require => Exec["apt-get update"]
   }
+
   File {
     require => Exec["apt-get update"]
   }
