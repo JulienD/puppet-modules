d7ec00dfcc35dcc1aaa0c457c16bb982052c5a00
Julien Dubreuil <dubreuil.julien@gmail.com>
2012-05-29 16:25:06 +0200

    updated code structure

--- modules/tools/manifests/init.pp
+++ modules/tools/manifests/init.pp
@@ -1,8 +1,16 @@
-class tools {
+# Class: tools
+#
+#   This class installs other usefull packages.
+#
+class tools::install {
 
   $packages = ["curl", "wget", "vim"]
 
   package { $packages:
     ensure => present,
   }
+}
+
+class tools {
+  include tools::install
 }
