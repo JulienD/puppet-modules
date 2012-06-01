302ea9357806d7b94e75c91b2e78c88ddd5ca949
Julien Dubreuil <dubreuil.julien@gmail.com>
2012-06-01 18:13:31 +0200

    added git class

---
+++ modules/git/manifests/init.pp
@@ -0,0 +1,29 @@
+# Class: git
+#
+#   This class installs git and add a config file containing aliases and colors
+#   definition...
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class git::install {
+  package { 'git-core':
+    ensure => present;
+  }
+}
+
+class git::config {
+  file {"/home/${username}/.gitconfig":
+    content => template('git/gitconfig.erb'),
+    require => Class['git::install'],
+  }
+}
+
+class git {
+  include git::install, git::config
+}
